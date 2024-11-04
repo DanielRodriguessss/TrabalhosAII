28fdb70c02072dcfed2e26090b0cc960386b51ce
DanielRodriguessss <a13676@esars.pt>
2024-11-04 12:14:07 +0000

    Adição das condições para as operações

--- calculadora.py
+++ calculadora.py
@@ -37,3 +37,30 @@ def calculadora():
         print("6 - Histórico")
 
         escolha= input("Digite sua escolha 1/2/3/4/5/6:")
+
+        if escolha != "6":
+            num1 = int(input("Digite o primeiro numero: "))
+            num2 = int(input("Digite o segundo numero: "))
+
+        if escolha == "1":
+            print(f"Resultado:  {num1} + {num2} = {soma(num1, num2)}")
+            historico.append(f"{num1} + {num2} = {soma(num1, num2)}")
+        elif escolha == "2":
+            print(f"Resultado: {num1} - {num2} = {subtração(num1, num2)}")
+            historico.append(f"{num1} - {num2} = {subtração(num1, num2)}")
+        elif escolha == "3":
+            print(f"Resultado: {num1} * {num2} = {multiplicação(num1, num2)}")
+            historico.append(f"{num1} * {num2} = {multiplicação(num1, num2)}")
+        elif escolha == "4":
+            print(f"Resultado: {num1} / {num2} = {divisão(num1, num2)}")
+            historico.append(f"{num1} / {num2} = {divisão(num1, num2)}")
+        if len(historico) > 5:
+            historico.pop(0)
+        elif escolha == "6":
+            ver_historico(historico)
+        elif escolha == "5":
+            print("A encerrar a calculadora")
+            break
+
+# Executa a calculadora
+calculadora()
